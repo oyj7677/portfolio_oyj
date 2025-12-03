d189152b9d2d1fccceadc4479726abc0001ca9c4
OYJ <oyj7677@gmail.com>
2025-12-03 21:23:40 +0900

    서비스 개발

--- src/main/kotlin/com/oyj/portfolio/presentation/service/PresentationService.kt
+++ src/main/kotlin/com/oyj/portfolio/presentation/service/PresentationService.kt
@@ -1,7 +1,45 @@
 package com.oyj.portfolio.presentation.service
 
+import com.oyj.portfolio.presentation.dto.IntroductionDTO
+import com.oyj.portfolio.presentation.dto.LinkDTO
+import com.oyj.portfolio.presentation.dto.ProjectDTO
+import com.oyj.portfolio.presentation.dto.ResumeDTO
+import com.oyj.portfolio.presentation.repository.PresentationRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class PresentationService {
+class PresentationService(
+    private val presentationRepository: PresentationRepository
+) {
+    @Transactional(readOnly = true)
+    fun getIntroductions(): List<IntroductionDTO> {
+        val introductions = presentationRepository.getActiveIntroductions()
+
+        return introductions.map { IntroductionDTO(it) }
+    }
+
+    @Transactional(readOnly = true)
+    fun getLinks(): List<LinkDTO> {
+        val links = presentationRepository.getActiveLinks()
+        return links.map { LinkDTO(it) }
+    }
+
+    @Transactional(readOnly = true)
+    fun getResume(): ResumeDTO {
+        val experiences = presentationRepository.getActiveExperiences()
+        val achievements = presentationRepository.getActiveAchievements()
+        val skills = presentationRepository.getActiveSkills()
+        return ResumeDTO(
+            experience = experiences,
+            achievements = achievements,
+            skills = skills
+        )
+    }
+
+    @Transactional(readOnly = true)
+    fun getProjects(): List<ProjectDTO> {
+        val projects = presentationRepository.getActiveProjects()
+        return projects.map { ProjectDTO(it) }
+    }
 }

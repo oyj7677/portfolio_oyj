f0a9c471e481d08de912795285aa5af36ca059b9
OYJ <oyj7677@gmail.com>
2025-12-16 22:23:08 +0900

    인터셉터 개발

--- src/main/kotlin/com/oyj/portfolio/presentation/controllerinterceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/oyj/portfolio/presentation/controllerinterceptor/PresentationInterceptor.kt
@@ -1,7 +1,25 @@
 package com.oyj.portfolio.presentation.controllerinterceptor
 
+import com.oyj.portfolio.domain.entity.HttpInterface
+import com.oyj.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
-class PresentationInterceptor {
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
+
 }
